b4e4303b4bbf4b852fbe48fef003e7cc6fe8e4ce
gorouter <cloudaple@gmail.com>
2018-11-04 21:51:01 +0800

    Update download_asuswrt.sh

--- asuswrt/download_asuswrt.sh
+++ asuswrt/download_asuswrt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/gorouter/src_asuswrt-ac828.git
-mv ./src_asuswrt-ac828 /home/ASUSWRT/asuswrt
+git clone https://github.com/foundations/asuswrt-ac828.git
+mv ./asuswrt-ac828 /home/ASUSWRT/asuswrt
 rm -rf /home/ASUSWRT/asuswrt/.git
