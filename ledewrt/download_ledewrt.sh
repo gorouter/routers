628a5174da58c995e0f000078e681bc89520abe9
gorouter <cloudaple@gmail.com>
2018-11-04 12:48:16 +0800

    Update download_ledewrt.sh

--- ledewrt/download_ledewrt.sh
+++ ledewrt/download_ledewrt.sh
@@ -2,10 +2,18 @@
 
 git clone https://github.com/coolsnowwolf/lede.git 
 mv ./lede /home/LEDEWRT/ledewrt
+
+#暂保留git以便重新下载源代码
 #rm -rf /home/LEDEWRT/ledewrt/.git
 
 cd /home/LEDEWRT/ledewrt
-#git checkout f09c4d358773e212d80f186b51273b1332e35de8 #adf2b2271359cd273ec8583805f999d5bb785b66
 
-#./scripts/feeds update -a 
-#./scripts/feeds install -a
+#可以在这里指定编译版本
+#git checkout f09c4d358773e212d80f186b51273b1332e35de8
+
+./scripts/feeds update -a 
+./scripts/feeds install -a
+
+#选择好router设置后执行以下命令进行编译(-j1 后面是线程数)
+#make menuconfig
+#make -j1 V=s 
